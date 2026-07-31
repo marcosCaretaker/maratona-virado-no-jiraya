3c33f35b9f68342ddf7ab99826144c4a7dab718a
Marcos Gabriel Goncalves do Nascimento <marcoscaretaker@gmail.com>
2026-07-31 11:50:39 -0300

    Adiciona exercícios de classes utilitárias,Scanner e início em file

---
+++ src/academy/marcoscaretaker/maratonajava/Uregex/ScannerTest01.java
@@ -0,0 +1,12 @@
+package academy.marcoscaretaker.maratonajava.Uregex;
+
+public class ScannerTest01 {
+    public static void main(String[] args) {
+        String text = "Levi, Eren, Mikasa, true, 200";
+        String[] names = text.split(",");
+        for (String name : names) {
+            System.out.println(name.trim());
+        }
+
+    }
+}
---
+++ src/academy/marcoscaretaker/maratonajava/Vio/test/FileTest01.java
@@ -0,0 +1,28 @@
+package academy.marcoscaretaker.maratonajava.Vio.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+
+public class FileTest01 {
+    public static void main(String[] args) {
+        File file = new File("text.txt");
+        try {
+            boolean isCreated = file.createNewFile();
+            System.out.println("Created: "+isCreated);
+            System.out.println("Path: "+file.getPath());
+            System.out.println("Absolute path: "+file.getAbsolutePath());
+            System.out.println("Is directory: "+file.isDirectory());
+            System.out.println("Is file: "+file.isFile());
+            System.out.println("Is hidden: "+file.isHidden());
+            System.out.println("Last modified: "+ Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()));
+            boolean exists = file.exists();
+            if(exists){
+                System.out.println("Deleted: "+file.delete());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
